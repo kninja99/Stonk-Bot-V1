719c3e477cd2ae976a76020a69a742d725229e49
Kninja99 <kcdamschen@yahoo.com>
2021-06-28 15:27:55 -0700

    Update functions.js
    js file now has two function, one that will add a ticker to the first header and a function that can add articles in the first ticker

--- web/js/functions.js
+++ web/js/functions.js
@@ -1,10 +1,19 @@
-function addTicker(text) {
-  // grabs ticker
-  var ticker = document.getElementById('ticker');
-  // text
-  ticker.innerHTML += text;
+// base function we can use to add only one ticker at the moment
+// just trying to get functionality down before optimizing for expansion
+function addTicker(tick) {
+  let h3 = document.querySelector('.ticker');
+  h3.textContent = tick;
 }
 eel.expose(addTicker);
 
-// this is used to test if the script is working or not
-addTicker('hello');
+// script to set news articles, takes in an array of strings
+// this function will also need work in order to expand the app
+function setNewsArticles(newArticleArr) {
+  let list = document.querySelector('.articles');
+  for (const text of newArticleArr) {
+    let article = document.createElement('li');
+    article.textContent = text;
+    list.appendChild(article);
+  }
+}
+eel.expose(setNewsArticles);
