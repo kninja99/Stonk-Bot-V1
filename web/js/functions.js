0eeb78a1cb3e2d48c2cce4c68c0f2e5542cc8c1d
Kninja99 <kcdamschen@yahoo.com>
2021-06-28 16:23:26 -0700

    Update functions.js
    condensed the two functions adding stock information into one, can also add more then one stock in dynamically now

--- web/js/functions.js
+++ web/js/functions.js
@@ -1,19 +1,30 @@
-// base function we can use to add only one ticker at the moment
-// just trying to get functionality down before optimizing for expansion
-function addTicker(tick) {
-  let h3 = document.querySelector('.ticker');
-  h3.textContent = tick;
-}
-eel.expose(addTicker);
-
-// script to set news articles, takes in an array of strings
-// this function will also need work in order to expand the app
-function setNewsArticles(newArticleArr) {
-  let list = document.querySelector('.articles');
-  for (const text of newArticleArr) {
-    let article = document.createElement('li');
-    article.textContent = text;
-    list.appendChild(article);
+/**
+ * funtion that will add a stock ticker and its respected news articles
+ * @param {string} tick - the ticker symbol in terms of a string
+ * @param {array of type string} articlesArr - array of strings that represent the news articles
+ */
+function addStock(tick, articlesArr) {
+  // first target the container for all stock related items
+  let container = document.querySelector('.container');
+  // div that holds all the stocks elements in that row
+  let div = document.createElement('div');
+  div.className = 'row';
+  // ticker implementation
+  let ticker = document.createElement('h3');
+  ticker.className = 'ticker';
+  ticker.textContent = tick;
+  div.appendChild(ticker);
+  // list implementation
+  let list = document.createElement('ul');
+  list.className = 'articles';
+  // for loop tha builds up the list element
+  for (const text of articlesArr) {
+    let articles = document.createElement('li');
+    articles.textContent = text;
+    list.appendChild(articles);
   }
+  div.appendChild(list);
+  //finally appends everything onto the container
+  container.appendChild(div);
 }
-eel.expose(setNewsArticles);
+eel.expose(addStock);
