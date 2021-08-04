3452f42843baacfd47356d9e972f63a9777f8c82
Kninja99 <kcdamschen@yahoo.com>
2021-08-04 13:02:08 -0700

    Search bar and button connecton
    connected the search bar to speak to the back end

--- main.py
+++ main.py
@@ -19,86 +19,32 @@ def grabInput(input):
     user_input_que.append(input)
 
 
-'''
-this function will return the headers of the articles to an array
-# param HTMLarr = articles list in html
-# return an array of type string, articleHeaderArr
-'''
-
-
-def articlesHeaderToString(HTMLarr):
-    articleHeaderArr = []
-    for i in HTMLarr:
-        header = i.find('h3')
-        articleHeaderArr.append(header.get_text())
-    return articleHeaderArr
-
-
-'''
-this function will return the preview of the articles to an array
-# param HTMLarr = articles list in html
-# return articlePreArr = the preview of the article
-'''
-
-
-def articlesPreviewToString(HTMLarr):
-    articlePreArr = []
-    for i in HTMLarr:
-        preview = i.find('p')
-        articlePreArr.append(preview.get_text())
-    return articlePreArr
-
-
-'''
-this function will build news articles on startup and also used for live updates
-# param stockTicker = ticker that you want to scrape info on
-'''
-
-
-def buildNews(stockTicker):
-    ticker = stockTicker.upper()
-    # builds link based off of the inputed ticker, currently just yahoo
-    yahoo_search = "https://finance.yahoo.com/quote/{tick}?p={tick}&.tsrc=fin-srch".format(
-        tick=ticker)
-    # gets html info and sends to a scraper called soup
-    html_info = requests.get(yahoo_search).text
-    soup = BeautifulSoup(html_info, "lxml")
-    # builds a news articles array
-    news_articles = soup.find('li', class_='js-stream-content Pos(r)')
-    # checks for a valid stock ticker, if not valid will spit out an error message
-    if news_articles == None:
-        eel.searchError()
-    else:
-        # sends info to frontend
-        eel.addStock(ticker, articlesHeaderToString(
-            news_articles), articlesPreviewToString(news_articles))
-
-
 # points eel to the directors where html file is
 eel.init('web')
 
-news_scraper = scraper.Scraper('DIS')
+# this is for testing
+news_scraper = scraper.Scraper('dis')
 # testing to see if new_scraper built right and is displaying data in order
-print('---- Headers ----')
-print(news_scraper.header_arr)
-print('---- article previews ----')
-# probably should look into a way of condensing market watch to only one element, so finding a way to grab the importent preview element of every ticker that is searched
-for arr in news_scraper.articles_info:
-    print('--news article--')
-    print(arr)
-print('---- links ----')
-print(news_scraper.link_to_article)
+# print('---- Headers ----')
+# print(news_scraper.header_arr)d
+# print('---- article previews ----')
+# # probably should look into a way of condensing market watch to only one element, so finding a way to grab the importent preview element of every ticker that is searched
+# for arr in news_scraper.articles_info:
+#     print('--news article--')
+#     print(arr)
+# print('---- links ----')
+# print(news_scraper.link_to_article)
 
 
 # starts the eel program
-eel.start('index.html', size=(1280, 720), position=(100, 40), block=False)
+eel.start('index.html', block=False)
 
 
 # application loop
 while True:
     if(len(user_input_que) > 0):
         # Takes in user input and will build the news for it
-        buildNews(user_input_que.pop(0))
+        print(user_input_que.pop(0))
     else:
         eel.sleep(1)
 
--- web/js/functions.js
+++ web/js/functions.js
@@ -1,43 +1,5 @@
-const searchButton = document.querySelector('button');
+const searchButton = document.querySelector('#search_button');
 const inputForm = document.querySelector('input');
-
-/**
- * funtion that will add a stock ticker and its respected news articles
- * @param {string} tick - the ticker symbol in terms of a string
- * @param {array of type string} articlesArr - array of strings that represent the news articles
- */
-function addStock(tick, headerArr, previewArr) {
-  // first target the container for all stock related items
-  let container = document.querySelector('.container');
-  // div that holds all the stocks elements in that row
-  let div = document.createElement('div');
-  div.className = 'row';
-  // ticker implementation
-  let ticker = document.createElement('h3');
-  ticker.className = 'ticker';
-  ticker.textContent = tick;
-  div.appendChild(ticker);
-  // list implementation
-  let list = document.createElement('ul');
-  list.className = 'articles';
-  // for loop tha builds up the list element
-  for (let i = 0; i < headerArr.length; i++) {
-    let li = document.createElement('li');
-    let h5 = document.createElement('h5');
-    h5.textContent = headerArr[i];
-    h5.className = 'article-header';
-    let p = document.createElement('p');
-    p.className = 'article-preview';
-    p.textContent = previewArr[i];
-    li.appendChild(h5);
-    li.appendChild(p);
-    list.appendChild(li);
-  }
-  div.appendChild(list);
-  //finally appends everything onto the container
-  container.appendChild(div);
-}
-eel.expose(addStock);
 
 /**
  * This function will be handling the search bar inputs
